7998b0dd18e24e1297e5df88e9c408fbf1f7097f
FlyingArs <davidyu89tech@gmail.com>
2015-10-05 11:08:02 -0400

    Changes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home', message:'David\'s Homepage' });
 });
 
+/* GET about page */
+router.get('/about', function(req, res, next){
+	res.render('about', { title: 'About', message:'David\'s About Page' });
+});
+
+/* GET projects page */
+router.get('/projects', function(req, res, next){
+	res.render('projects', { title: 'Proejct', message:'David\'s Proejct Page' });
+});
+
+/* GET services page */
+router.get('/services', function(req, res, next){
+	res.render('services', { title: 'Services', message:'David\'s Services Page' });
+});
+
+/* GET contact page */
+router.get('/contact', function(req, res, next){
+	res.render('contact', { title: 'Contact', message:'David\'s Contact Page' });
+});
+
 module.exports = router;
