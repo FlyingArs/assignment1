2c513a0e94c1fa1dbe7a8f8dce3ac4e1114398b0
FlyingArs <davidyu89tech@gmail.com>
2015-10-12 22:12:36 -0400

    Changes

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+//delcare the use of public folder
+//app.use(express.static(__dirname + '/public'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', message:'David\'s Homepage' });
+  res.render('index', { title: 'Home', message:'David\'s World' });
 });
 
 /* GET about page */
