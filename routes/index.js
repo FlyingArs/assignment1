19b7bb746e627878b69b9197ef4ff7ae0478a58f
FlyingArs <davidyu89tech@gmail.com>
2015-10-14 09:47:23 -0400

    Install nodemailer and other changes

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', message:'David\'s World' });
+  res.render('index', { title: 'Home', message:'Donald\'s World' });
 });
 
 /* GET about page */
 router.get('/about', function(req, res, next){
-	res.render('about', { title: 'About', message:'David\'s About Page' });
+	res.render('about', { title: 'About' });
 });
 
 /* GET projects page */
 router.get('/projects', function(req, res, next){
-	res.render('projects', { title: 'Proejct', message:'David\'s Proejct Page' });
+	res.render('projects', { title: 'Projects'});
 });
 
 /* GET services page */
 router.get('/services', function(req, res, next){
-	res.render('services', { title: 'Services', message:'David\'s Services Page' });
+	res.render('services', { title: 'Services' });
 });
 
 /* GET contact page */
 router.get('/contact', function(req, res, next){
-	res.render('contact', { title: 'Contact', message:'David\'s Contact Page' });
+	res.render('contact', { title: 'Contact'});
 });
+
+/* POST contact page */
+
+
 
 module.exports = router;
