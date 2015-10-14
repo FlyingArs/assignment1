71fb46f4cd1f500d74a17789462a3055b0013c82
FlyingArs <200286902@BA-LC3-1825.student.georgianc.on.ca>
2015-10-14 15:30:06 -0400

    Change

--- routes/index.js
+++ routes/index.js
@@ -33,25 +33,27 @@ router.get('/contact', function(req, res, next){
 //using nodemailer to send email when user hit the "submit" button 
 
 //create a mail transporter
-var transporter = nodemail.createTransport("SMTP", {
-    service: "Gmail"
+var transporter = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
     auth: {
-        user: "xxx@gmail.com",
-        pass: "userpassword"   
+        user: "flyingars@gmail.com",
+        pass: "October19*"   
     }    
 });   
     
 //create the POST method
-router.POST('/contact', function(req, res, next){
+router.post('/contact', function(req, res, next){
+    
 //specify the mail option   
 var mailOptions = {
-    from: 'David Yu <davidyu89tech@gmail.com',
-    to: 'davidyu89tech@gmail.com',
+    from: 'flyingars@gmail.com',
+    to: 'flyingars@gmail.com',
     subject: 'New Business Idea',
-    html: '<h2>Contact Request</h2>' +
-          '<p>'+req.body.name+'</p>' +
-          '<p>'+req.body.email+'</p>' +
-          '<p>'+req.body.message+'</p>'
+    text:req.body.name+req.body.email+req.body.message 
+          //'<h2>Contact Request</h2>' +
+          //'<p>'+req.body.name+'</p>' +
+          //'<p>'+req.body.email+'</p>' +
+          //'<p>'+req.body.message+'</p>'
         
     
 };
@@ -59,14 +61,14 @@ var mailOptions = {
 //transporter sends the email
 transporter.sendMail(mailOptions, function(error, response){
     if(err){
-        res.end(err.toString);   
+        res.render('contact', {title: 'Wrong Input', msg: 'Something happend'});   
     }
     else{
-        res.render("Thank you for your information");
+        res.render('contact', {title: 'Correc Input', msg: 'Thank you for your information'});
     }
     
 });
     
-}
+});
 
 module.exports = router;
