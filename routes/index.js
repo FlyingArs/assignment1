9aa3d284418d83c23470f2175ac6f105b6e82026
FlyingArs <davidyu89tech@gmail.com>
2015-10-14 10:19:45 -0400

    Working on nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+//create a nodemailer object
+var nodemailer = require('nodemailer');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home', message:'Donald\'s World' });
@@ -27,7 +30,19 @@ router.get('/contact', function(req, res, next){
 });
 
 /* POST contact page */
+//using nodemailer to send email when user hit the "submit" button 
+router.get('/contact', function(req, res, next){
 
+//create a mail transporter
+var mailOptions = {
+    from: 'David Yu <davidyu89tech@gmail.com',
+    to: 'davidyu89tech@gmail.com',
+    subject: 'New Business Idea',
+    html: '<h2>'+req.email+</h2>
+    
+};
 
+//specify the mail option
+});
 
 module.exports = router;
